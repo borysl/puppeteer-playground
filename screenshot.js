9ff5e2ccef76f9b0ab834a339643bc9d6a038b0c
borysl <borys.lebeda@gmail.com>
2018-06-20 19:18:28 +0300

    maximize screen

--- screenshot.js
+++ screenshot.js
@@ -1,8 +1,17 @@
+'use strict';
+
 const puppeteer = require('puppeteer');
 
 async function takeScreenshot(url, fileName, showBrowser) {
-  const browser = await puppeteer.launch({headless:!showBrowser});
+  const browser = await puppeteer.launch({
+    headless: !showBrowser,
+    args: ['--start-maximized'],
+  });
   const page = await browser.newPage();
+  let width = await page.evaluate(async () => await window.screen.availWidth);
+  let height = await page.evaluate(async () => await window.screen.availHeight);
+
+  await page.setViewport({ width, height });
   await page.goto(url);
   await page.screenshot({path: fileName});
 
