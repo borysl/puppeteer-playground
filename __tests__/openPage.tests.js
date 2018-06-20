b180f3da575ab9fa7de43b2313fb068f67626d02
borysl <borys.lebeda@gmail.com>
2018-06-20 20:51:12 +0300

    Add the second scenario

--- __tests__/openPage.tests.js
+++ __tests__/openPage.tests.js
@@ -1,8 +1,18 @@
+'use strict';
+
 const openPage = require('../openPage');
 
 describe('myHeritage suite', () => {
   test('Opening main page', async () => {
     const {browser, page } = await openPage('https://www.myheritage.com/');
+    await browser.close();
+  }, 10000);
+  test('Order DNA test', async () => {
+    const {browser, page } = await openPage('https://www.myheritage.com/');
+    const navigationPromise = page.waitForNavigation();
+    await page.click('#top_get_dna_test_button');
+    await navigationPromise;
+    await   page.screenshot({path: 'DNATestProposal.png'})
     await browser.close();
   }, 10000);
 })
