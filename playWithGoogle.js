1308c282e8dda85567b78cee7d37b92f7f4816e3
borysl <borys.lebeda@gmail.com>
2018-06-20 19:54:44 +0300

    Play scenario with use of promises

---
+++ playWithGoogle.js
@@ -0,0 +1,28 @@
+'use strict';
+const { openSite } = require('./openSite');
+const SHOW_BROWSER = true;
+
+async function playScenario(page) {
+  await page.type('.gsfi', 'lego mindstorms spik3r');
+  const navigationPromise = page.waitForNavigation();
+  await page.click('input[type=submit]');
+  await navigationPromise;
+  await page.screenshot({path: 'searchResult.png'});
+}
+
+function playWithGoogle() {
+  console.log('Take some action in google');
+  openSite('https://www.google.com', SHOW_BROWSER).then(({page, browser}) => {
+    console.log(`Yeah! I've got page: ${page}`);
+    let promise = playScenario(page, browser);
+    promise.then(() => {
+      console.log('Success!');
+    }).catch(err => {
+      console.log(`Got an error ${err}`);
+    }).finally(() => {
+      browser.close();
+    });
+  });
+}
+
+module.exports.playWithGoogle = playWithGoogle;
