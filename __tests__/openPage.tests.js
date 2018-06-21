f70b2c71ddce0bc5c3a6f89eb5d1e56eb5a97b3e
borysl <borys.lebeda@gmail.com>
2018-06-21 10:13:44 +0300

    Add google test suite

--- __tests__/openPage.tests.js
+++ __tests__/openPage.tests.js
@@ -12,7 +12,19 @@ describe('myHeritage suite', () => {
     const navigationPromise = page.waitForNavigation();
     await page.click('#top_get_dna_test_button');
     await navigationPromise;
-    await   page.screenshot({path: 'DNATestProposal.png'})
+    await page.screenshot({path: 'DNATestProposal.png'})
     await browser.close();
   }, 10000);
+})
+
+describe('google tests', () => {
+  test('Search for bunch of cash in google', async () => {
+    const { browser, page } = await openPage('https://www.google.com/');
+    await page.type('.gsfi', 'трімільйона налічнимі!')
+    const navigationPromise = page.waitForNavigation();
+    await page.click('input[type=submit]');
+    await navigationPromise;
+    await page.screenshot({path: 'searchResult.png'})
+    await browser.close();
+  })
 })
